d5ab1fee79f57dbfbecd1de49c6ffe708fbc1ac1
Travis Haley <travis@haleycomputersolutions.com>
2026-05-24 13:48:29 -0600

    feat(survival): move eat/drink commands from core to survival pack

--- packages/@tapestry/survival/scripts/commands/drink.js
+++ packages/@tapestry/survival/scripts/commands/drink.js
@@ -13,7 +13,7 @@ tapestry.commands.register({
         if (drinkable) {
             actor.send('You drink from ' + item.name + '.\r\n');
             actor.sendToRoom(actor.name + ' drinks from ' + item.name + '.\r\n');
-            var current = tapestry.consumables.getSustenance(actor.entityId);
+            var current = tapestry.world.getProperty(actor.entityId, 'sustenance') || 100;
             tapestry.world.setProperty(actor.entityId, 'sustenance', Math.min(100, current + 15));
             return;
         }
